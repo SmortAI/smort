462048de234e093c1c371b9755f031dd3a748987
itay-cyber <itaykag@gmail.com>
2021-03-05 08:41:22 +0100

    Done stff

--- cmds/misc/convert_currency.py
+++ cmds/misc/convert_currency.py
@@ -8,7 +8,12 @@ talker = SmortTTS()
 recognizer = SmortRecognizer()
 
 
-money_to_code = {"USD": "dollar", "EUR": "euro", "INR": "indian rupees"}
+money_to_code = {"USD": "dollar", "EUR": "euro", "INR": "indian rupees", "AFA": "ffghanistan afghani",
+                 "ALL": "albanien lek", "DZD": "algerian dinar", "AOR": "angolan kwanza reajustado",
+                 "ARS": "argentine peso", "AMD": "armenian dram", "AWG": "aruban guilder", "AUD": "australian dollar",
+                 "AZN": "azerbaijanian new manat", "BSD": "bahamian dollar", "BHD": "bahraini dinar", "BDT": "bangladeshi taka",
+                 "BBD": "barbados dollar", "BYN": "belarusian ruble", "BZD": "belize dollar", "BMD": "bermudian dollar",
+                 "BTN": "bhutan ngultrum", "BOB": "bolivian boliviano", "BWP": "botswana pula"}
 
 def convert_currency():
     pass
